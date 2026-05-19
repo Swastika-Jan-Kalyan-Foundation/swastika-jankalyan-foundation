e920fef8f023c1d86aee5d0f0251ee3251e09125
Gyan <gyan0304@gmail.com>
2026-05-19 05:02:49 -0400

    commented out the temporary imports

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import Footer from './components/Footer'
 import NavBar from './components/Navbar'
 import SwastikaChatbot from './components/Swastika'
 import Careers from './routes/Careers'
-import {VerifyCertificate} from './routes/VerifyCertificate'
+//import {VerifyCertificate} from './routes/VerifyCertificate'
 import TermsOfUse from './components/TermsOfUse'
 import PrivacyPolicy from './components/PrivacyPolicy'
 function App() {
@@ -37,7 +37,7 @@ function App() {
         <Route path='/trackyourimpact' element={<TrackImpact />} />
         <Route path='/annualreport' element={<AnnualReport />} />
         <Route path='/careers' element={<Careers />} />
-        <Route path='/verifycertificate' element={<VerifyCertificate />} />
+      {/*<Route path='/verifycertificate' element={<VerifyCertificate />} />*/}
         <Route path='/termsofuse' element={<TermsOfUse />} />
         <Route path='/privacypolicy' element={<PrivacyPolicy />} />
         </Routes>
